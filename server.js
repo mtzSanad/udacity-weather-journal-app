fa73338d02a8ff74f32e7f7b79234eba7d983ae9
mtzSanad <mtz.sanad@gmail.com>
2022-05-29 20:58:11 +0200

    finalize

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
-// Require Express to run server and routes
+// Require Express to run server and routes, body-barser and cors
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
+//Application Routing
+
+//Returning projectData object
+app.get("/getData", (req, res) => {
+  res.send(projectData);
+});
+
+app.post("/addData", (req, res) => {
+  console.log(11);
+  //Getting the data sent in request
+  const reqData = req.body;
+  //Spreading reqData properties into project data
+  projectData = { ...reqData };
+  res.send(projectData);
+});
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, () => {
+  console.log(`Application is running on port ${port}`);
+});
--- website/resources/js/app.js
+++ website/resources/js/app.js
@@ -1,5 +1,72 @@
 /* Global Variables */
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "&appid=4cfc4dcdb8fcb2a99a1a30a3cbe67a22&units=imperial";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
+
+//Sending request
+const postData = async (url = "", zip, apiKey) => {
+  const response = await fetch(url + zip + apiKey);
+
+  try {
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (e) {
+    console.error("error", e);
+  }
+};
+
+//Creating event listener for button
+//Selecting button element
+const generateBtn = document.querySelector("#generate");
+
+//Adding on click event listener
+generateBtn.addEventListener("click", async () => {
+  //Surrounding the block of code in a try catch
+  try {
+    //Getting the content data
+    const content = document.querySelector("#feelings").value;
+
+    //Getting zip code data
+    const zip = document.querySelector("#zip").value;
+
+    //Calling the api to get weather data
+    const data = await postData(baseUrl, zip, apiKey);
+    const temp = data.main.temp;
+
+    //Constructing the object data that will be sent to the server
+    //Hint: new object short object syntax is used
+    const projectData = {
+      temp,
+      date: newDate,
+      content,
+    };
+
+    //Sending the data to the server using post method
+    const response = await fetch("/addData", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(projectData),
+    });
+    //These are the data that is coming from post but we will not use it we will use the one getting from get request
+    const serverData = await response.json();
+
+    //Calling get request to get data
+    const getResponse = await fetch("/getData");
+    //Here we are having the object data
+    const allData = await getResponse.json();
+    console.log(allData);
+    //Populating html with the data
+    document.getElementById("temp").innerHTML =
+      Math.round(allData.temp) + " degrees";
+    document.getElementById("content").innerHTML = allData.content;
+    document.getElementById("date").innerHTML = allData.date;
+  } catch (error) {
+    console.error(error);
+  }
+});
